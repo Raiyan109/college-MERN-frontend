79f4f8ce15a6aa68340cb2e494a593abda102404
Raiyan109 <kraiyan109@gmail.com>
2023-08-04 21:00:36 +0600

    firebase api keys env local

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { FacebookAuthProvider, getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCnPiib70cGt0atpwUSEJgt2-hMZ7tj60A",
-    authDomain: "college-mern.firebaseapp.com",
-    projectId: "college-mern",
-    storageBucket: "college-mern.appspot.com",
-    messagingSenderId: "848899912321",
-    appId: "1:848899912321:web:22c8bbf9a20cafd7a45380"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
